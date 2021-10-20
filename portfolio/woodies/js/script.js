3a6271372ee872a723481d992098bf4bfc42e4a3
kssu <71457802+kssu@users.noreply.github.com>
2021-10-20 16:18:14 +0200

    Add files via upload

---
+++ portfolio/woodies/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.carousel').slick({
+		dots: true,
+		speed: 1200,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/previous.png">Previous</button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/next.png">Next</button>'
+	});
+	
+  });
+  
